817888af90caef831956de0aa53af981830e9798
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-05-30 15:22:26 +0300

    finished

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 respone = requests.get("https://stackoverflow.com/questions")
+soup = BeautifulSoup(respone.text, 'html.parser')
+
+questions = soup.select(".question-summary")
+for question in questions:
+    print(question.select_one(".question-hyperlink").getText())
+    print(question.select_one(".vote-count-post").getText())
